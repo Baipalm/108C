3535b6fc10b8d21a5dc4c4299e33a1c7abf3dbb5
Baipalm <ttamajai@ucsb.edu>
2025-06-04 14:48:02 -0700

    Update Cost_functions.py

--- pages/Cost_functions.py
+++ pages/Cost_functions.py
@@ -38,7 +38,7 @@ def run_nmf(V, cost):
 
 fro = run_nmf(V_interp, 'frobenius')
 kl = run_nmf(V_interp, 'kullback-leibler')
-error_fro = np.linalg.norm(V_terp-fro, 'fro')
+error_fro = np.linalg.norm(V_interp-fro, 'fro')
 error_kl = kl_divergence(V_interp,kl)
 
 # Title
